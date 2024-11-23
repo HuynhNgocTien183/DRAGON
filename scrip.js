c4928d67618545c97506cf5bcf81d5214fc9a329
HuynhNgocTien183 <tienhuynh7747@gmail.com>
2024-11-23 20:13:49 +0700

    done

---
+++ scrip.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+    $(".img,#myAudio").hide()
+
+    $("#play").click(function(){
+        $("#play").hide()
+
+        $("body").css({
+           
+           
+        })
+
+        $(".img,#myAudio").show()
+    })
+
+}
